e96c530ab376c3de2ea3f1e9b1b24aabbe9df278
Lucimeri Andretta <113333171+luandretta@users.noreply.github.com>
2023-02-17 08:20:40 +0000

    Feat: Add function to game.js

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+// {} because the exporting more than  one object and function from this file 
+module.exports = { game };
